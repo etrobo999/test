f90061744ffa49b8a5602a0635e5b21b4648c8ef
etrobo_kougaku2024 <k020c1394@g.neec.ac.jp>
2024-08-23 02:27:09 +0900

    Delete scenario_handler directory

--- scenario_handler/DoubleLoopScenario.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "Scenario2.h"
-class DoubleLoopScenario : public Scenario2 {
-public:
-    DoubleLoopScenario();
-    void execute() override;  // Execute double loop scenario
-};
