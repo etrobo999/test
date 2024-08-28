e5144c76f786a5a909d3423fbaf6bb556728babf
etrobo_kougaku2024 <k020c1394@g.neec.ac.jp>
2024-08-28 13:44:12 +0900

    Add files via upload

--- LineTracer/LineTracer.cpp
+++ LineTracer/LineTracer.cpp
@@ -523,7 +523,7 @@ void applyGrayWorldWhiteBalance(Mat& src) {
 /* フレームのトリミング＆HSV変換 */
 static tuple<Mat, Mat>  RectFrame(const Mat& frame) {
     Mat rectframe, hsv;
-    rectframe = frame(Rect(140, 240, 360, 80));
+    rectframe = frame(Rect(140, 240, 360, 40));
     cvtColor(rectframe, hsv, COLOR_BGR2HSV);
     return make_tuple(rectframe, hsv);
 }
