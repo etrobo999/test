ad82b5ee790cfd8b14fdaf7d9f14254ab251142f
etrobo_kougaku2024 <k020c1394@g.neec.ac.jp>
2024-08-30 15:47:16 +0900

    Add files via upload

--- LineTracer/LineTracer.cpp
+++ LineTracer/LineTracer.cpp
@@ -21,7 +21,7 @@ PID Bcurvetpid = {0.12, 0.005, 0, 0, 0}; //急カーブPID
 PID Mcurvetpid = {0.1, 0.004, 0, 0, 0}; //ちょうどいいカーブPID
 PID Scurvetpid = {0.09, 0.005, 0, 0, 0}; //ゆっくりカーブPID
 
-CameraSettings camera_settings = {1640, 1232, CV_8UC3, 15};
+CameraSettings camera_settings = {1640, 1232, CV_8UC3, 5};
 
 
 /*使用する変数の宣言*/
@@ -79,6 +79,7 @@ void* opencv_thread_func(void* arg) {
         Camera.set(cv::CAP_PROP_FRAME_HEIGHT, camera_settings.frame_height);
         Camera.set(cv::CAP_PROP_FORMAT, camera_settings.format);
         Camera.set(cv::CAP_PROP_FPS, camera_settings.fps);
+        Camera.set(cv::CAP_PROP_ZOOM, 1);
         if (!Camera.open()) {
             cerr << "Error: !Camera.open" << endl;
             pthread_exit(NULL);
