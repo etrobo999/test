9fbdf01b6e20b5c83850cd806b9952126af055dc
etrobo_kougaku2024 <k020c1394@g.neec.ac.jp>
2024-09-05 18:07:41 +0900

    Add files via upload

--- LineTracer/LineTracer.cpp
+++ LineTracer/LineTracer.cpp
@@ -19,8 +19,8 @@ raspicam::RaspiCam_Cv Camera;
 /*PIDインスタンス生成*/
 PID straightpid = {0.06, 0, 0.005, 0, 0}; //ストレートPID
 PID Bcurvetpid = {0.12, 0.004, 0, 0, 0}; //急カーブPID
-PID Mcurvetpid = {0.8, 0.003, 0, 0, 0}; //ちょうどいいカーブPID
-PID Scurvetpid = {0.8, 0.004, 0, 0, 0}; //ゆっくりカーブPID
+PID Mcurvetpid = {0.08, 0.003, 0, 0, 0}; //ちょうどいいカーブPID
+PID Scurvetpid = {0.08, 0.004, 0, 0, 0}; //ゆっくりカーブPID
 
 /*rectの値初期化*/
 int rect_x = 0;
