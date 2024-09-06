2ec5222a9d685d86510a76fb4fd261aa090c91eb
etrobo_kougaku2024 <k020c1394@g.neec.ac.jp>
2024-09-06 13:01:00 +0900

    Add files via upload

--- LineTracer/LineTracer.cpp
+++ LineTracer/LineTracer.cpp
@@ -207,7 +207,7 @@ void* display_thread_func(void* arg) {
 //        }
         // 表示処理
         cv::imshow("temp_frame1", temp_frame1);
-        cv::imshow("frame", frame);
+        cv::imshow("orizin_frame", orizin_frame);
         cv::imshow("mask", mask);
         cv::waitKey(1);
 
