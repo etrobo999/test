3f82c6b2996c21a3697a6327c324e83c8b19615c
etrobo_kougaku2024 <k020c1394@g.neec.ac.jp>
2024-09-06 17:15:35 +0900

    Add files via upload

--- LineTracer/LineTracer.cpp
+++ LineTracer/LineTracer.cpp
@@ -373,7 +373,7 @@ void* main_thread_func(void* arg) {
             PIDMotor(Bcurvetpid);
             std::cout <<ev3_motor_get_counts(left_motor)<< std::endl;
             std::cout <<ev3_motor_get_counts(right_motor)<< std::endl;
-            if(ev3_motor_get_counts(left_motor) + ev3_motor_get_counts(right_motor) >= 13600){
+            if(ev3_motor_get_counts(left_motor) + ev3_motor_get_counts(right_motor) >= 13400){
                 scene++;
             }
             std::cout << "Case 18" << std::endl;
